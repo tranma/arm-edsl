a7576b0f0d5054e198afc59b38ce825be43bd1e0
Tran Ma <tran@defma.in>
2014-04-22 01:38:57 +1000

    wibble

--- ARM/Utils/TH.hs
+++ ARM/Utils/TH.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
+-- | Generate code used elsewhere in the package.
 module ARM.Utils.TH
        ( genClassExclude
+       , genClassBounded
        , genFamilySucc )
        where
 
@@ -19,13 +21,31 @@ decClass t cname = do
   tv   <- newName "a"
   k    <- liftM (KindedTV tv) $ conT t
   classD (cxt []) cname [k] [] []
-  
-decInstance :: Name -> Name -> Q Dec
-decInstance cname c = instanceD (cxt []) (appT (conT cname) (promotedT c)) []
+
+decIns :: Name -> Q Type -> Q Dec
+decIns cname t = instanceD (cxt []) (appT (conT cname) t) []
+
+decInsPromoted :: Name -> Name -> Q Dec
+decInsPromoted cname c = decIns cname (promotedT c)
 
 -- | Guarantee uniqueness yourself
 prefixName :: String -> Name -> Name
 prefixName p n = mkName $ p ++ (nameBase n)
+
+
+genClassBounded :: String -> Int -> Name -> Q [Dec]
+genClassBounded s i tname
+  | i < 2 = error $ "at least 2"
+  | otherwise = do
+      TyConI (DataD _ _ _ ((NormalC zn [])
+                          :(NormalC sn [(_, ConT _)])
+                          :[]) _) <- reify tname
+      let cname = mkName s
+      let mkSuccT p = appT (promotedT sn) p
+      let succs = take i $ iterate mkSuccT $ promotedT zn
+      cdec <- decClass tname cname
+      cins <- mapM (decIns cname) succs
+      return $ cdec:cins
 
 -- | Generate a typeclass that excludes one constructor of a datatype
 --   (the type must be promotable, so no GADTs yet), e.g.
@@ -40,7 +60,7 @@ genClassExclude t con = do
   TyConI (DataD _ _ _ cons _) <- reify t
   let cname = prefixName "Not" con
   cdec <- decClass t cname
-  cins <- mapM (decInstance cname) $ delete con $ map conName cons
+  cins <- mapM (decInsPromoted cname) $ delete con $ map conName cons
   return $ cdec:cins
 
 -- | Generate a type family that yields the next constructor
